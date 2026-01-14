49b2c066b1142cf37b493158684cf195b609268b
gwozdziu1017 <gwozdziu1017@o2.pl>
2026-01-14 20:40:46 +0100

    Initial Commit

---
+++ WhackAMole/WhackAMoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhackAMoleApp.swift
+//  WhackAMole
+//
+//  Created by Damian Gwóźdź on 14/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct WhackAMoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
